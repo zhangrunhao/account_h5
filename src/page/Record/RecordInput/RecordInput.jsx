07860172ed2f6cb891876c5355c2b22bb7649f60
zhangrh <runhaozhang@sohu-inc.com>
2021-04-23 11:47:50 +0800

    feat: reacord basic style

--- src/page/Record/RecordInput/RecordInput.jsx
+++ src/page/Record/RecordInput/RecordInput.jsx
@@ -1,35 +1,67 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const Wrapper = styled.div`
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 6.2rem;
+  background-color: seashell;
+`
+
+const SumOfMoney = styled.div`
+  background-color: rebeccapurple;
+  width: 100%;
+  height: 1rem;
+  line-height: 1rem;
+  font-size: .6rem;
+  color: red;
+  text-align: right;
+`
+
+const ToolList = styled.div`
+  background-color: pink;
+  display: flex;
+  justify-content: space-around;
+`
+
+const Tool = styled.button`
+`
+
+const InputMain = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
+
+const InputButton = styled.div`
+  width: 1.875rem;
+  height: 1.16rem;
+  line-height: 1.16rem;
+  font-size: .4rem;
+  text-align: center;
+  background-color: lavenderblush;
+`
+
+const inputMainButtonArrayInfo = [1, 2, 3, "删除", 4, 5, 6, "-", 7, 8, 9,  "+", "再记", 0, ".", "保存"]
 export default class RecordInput extends React.Component {
   render () {
     return (
-      <div className="record-input">
-        {/* 总金额 */}
-        <div className="num">0</div>
-        {/* 工具列表 */}
-        <div className="tool-list">
-          <button className="choose-account">账户</button>
-          <button className="choose-date">今天11:48</button>
-          <button className="remark-input">备注</button>
-        </div>
-        {/* 输入区 */}
-        <div className="input-main">
-          输入区
-          <ul className="number-list">
-            <li>0</li>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-          </ul>
-          <ul className="button-list">
-            <li>删除</li>
-            <li>+</li>
-            <li>-</li>
-            <li>保存</li>
-          </ul>
-        </div>
-      </div>
+      <Wrapper>
+        <SumOfMoney>100</SumOfMoney>
+        <ToolList>
+          <Tool>账户</Tool>
+          <Tool>今天</Tool>
+          <Tool>备注</Tool>
+        </ToolList>
+        <InputMain>
+          {
+            inputMainButtonArrayInfo.map((v, i) => 
+              <InputButton key={v}>{v}</InputButton>
+            )
+          }
+        </InputMain>
+      </Wrapper>
     )
   }
 }
--- src/page/Record/TypeChoose/TypeChoose.jsx
+++ src/page/Record/TypeChoose/TypeChoose.jsx
@@ -1,30 +1,44 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const TypeChooseWrapper = styled.div`
+ background-color: pink;
+ display: flex;
+ flex-wrap: wrap;
+`
+
+const TypeChooseItem = styled.div`
+  background-color: skyblue;
+  width: 1.5rem;
+  height: .8rem;
+`
+
+const TypeIcon = styled.div`
+  background-color: khaki;
+  margin: 0 auto;
+  width: .4rem;
+  height: .4rem;
+`
+
+const TypeName = styled.div`
+width: 100%;
+padding-top: .06rem;
+text-align: center;
+`
 
 export default class TypeChoose extends React.Component {
   render () {
     return (
-      <div className="type-choose">
-        <ul>
-          <li>
-            <div className="icon">
-              <img src="" alt=""/>
-            </div>
-            <div className="name">三餐</div>
-          </li>
-          <li>
-            <div className="icon">
-              <img src="" alt=""/>
-            </div>
-            <div className="name">三餐</div>
-          </li>
-          <li>
-            <div className="icon">
-              <img src="" alt=""/>
-            </div>
-            <div className="name">三餐</div>
-          </li>
-        </ul>
-      </div>
+      <TypeChooseWrapper>
+        {
+          Array(22).fill("三餐").map((v, i) => 
+            <TypeChooseItem key={i}>
+              <TypeIcon></TypeIcon>
+              <TypeName>{v}</TypeName>
+            </TypeChooseItem>
+          )
+        }
+      </TypeChooseWrapper>
     )
   }
 }
