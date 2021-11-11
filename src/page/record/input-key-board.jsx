5f355d2c3901c02424e0a8a1def2d615ac56602a
zhangrh <runhaozhang@sohu-inc.com>
2021-11-11 17:42:29 +0800

    feat: complete key board - and +

--- src/page/record/input-key-board.jsx
+++ src/page/record/input-key-board.jsx
@@ -51,37 +51,144 @@ export default class InputKeyBoard extends React.Component {
   }
   handleKeyBoardClick(key) {
     if (_.isNumber(key)) {
-      // 数字
-      money += key;
+      // 数字 非0
       if (
-        money.indexOf(".") > -1 &&
-        money.substring(money.indexOf(".")).length > 3
+        money.indexOf(".") > -1 && //  存在.
+        money.indexOf("+") < 0 && // 不存在+
+        money.indexOf("-") < 0 &&  // 不存在-
+        money.substring(money.lastIndexOf(".")).length > 2 // 小数点后, 最多两位
       ) {
         return;
       }
-      if (money.length > 1 && money.indexOf("0") === 0) {
-        money = money.substring(1);
+
+      if (
+        money.indexOf(".") > -1 && //存在.
+        money.indexOf("+") > -1 && // 存在 +
+        money.lastIndexOf(".") > money.lastIndexOf("+") && // 最后. 比 最后+ 更靠后
+        money.substring(money.lastIndexOf(".")).length > 2 // 最后一个. 后面最多两位
+      ) {
+        return;
       }
-      this.props.moneyChange(money);
+
+      if (
+        money.indexOf(".") > -1 && //存在.
+        money.indexOf("-") > -1 && // 存在 -
+        money.lastIndexOf(".") > money.lastIndexOf("-") && // 最后. 比 最后+ 更靠后
+        money.substring(money.lastIndexOf(".")).length > 2 // 最后一个. 后面最多两位
+      ) {
+        return;
+      }
+
+      if (money === "0") {
+        // 只有一位, 并且第一位是0
+        money = "";
+      }
+      money += key;
     } else if (key === ".") {
-      // 小数点
-      if (money.indexOf(".") > -1) return;
+      //小数点
+      if (
+        money.indexOf(".") > -1 && // 存在.
+        money.indexOf("+") < 0 && // 不存在+
+        money.indexOf("-") < 0  // 不存在-
+      ) {
+        return;
+      }
+
+      if (
+        money.indexOf(".") > -1 && // 存在.
+        money.indexOf("+") > -1 && // 存在+
+        money.lastIndexOf(".") > money.lastIndexOf("+") // 最后一个. 在最后一个+ 的后面
+      ) {
+        return;
+      }
+
+      if (
+        money.indexOf(".") > -1 && // 存在.
+        money.indexOf("-") > -1 && // 存在-
+        money.lastIndexOf(".") > money.lastIndexOf("-") // 最后一个. 在最后一个- 的后面
+      ) {
+        return;
+      }
+
+      if (
+        money.indexOf("+") > -1 && // 存在+
+        money.length === money.lastIndexOf("+") + 1 // 最后一位是 +
+      ) {
+        return;
+      }
+
+      if (
+        money.indexOf("-") > -1 && // 存在-
+        money.length === money.lastIndexOf("-") + 1 // 最后一位是 -
+      ) {
+        return;
+      }
+
       money += key;
     } else if (key === "删除") {
       // 删除
       money = money.slice(0, -1);
-      if (money.length < 1) money = "0";
-      this.props.moneyChange(money);
+
+      if (money.length < 1) {
+        money = "0";
+      }
     } else if (key === "-") {
-      // TODO: handle -
+      // -
+      if (money.indexOf('-') > -1) {
+        // 存在 -
+        const arr = money.split('-'); // 先计算
+        money = Number(arr[0]) - Number(arr[1]) + ""
+      }
+
+      if (money.indexOf("+") > -1) {
+        // 存在+
+        const arr = money.split("+"); // 先计算
+        money = Number(arr[0]) + Number(arr[1]) + "";
+        if (Number(money) < 0) money = "0"; // 如果 - 完, 小于0, 就等于0
+      }
+
+      if (
+        money.indexOf(".") > -1 && // 存在.
+        money.lastIndexOf(".") + 1 === money.length // .在最后一位
+      ) {
+        money = money.substring(0, money.lastIndexOf(".")); // 截取最后一位.
+      }
+
+      money += '-'
     } else if (key === "+") {
-      // TODO: handle +
+      // +
+      if (money.indexOf("+") > -1) {
+        // 存在+
+        const arr = money.split("+"); // 先计算
+        money = Number(arr[0]) + Number(arr[1]) + "";
+      }
+
+      if (money.indexOf('-') > -1) {
+        // 存在 -
+        const arr = money.split('-'); // 先计算
+        money = Number(arr[0]) - Number(arr[1]) + ""
+        if (Number(money) < 0) money = "0"; // 如果 - 完, 小于0, 就等于0
+      }
+
+      if (
+        money.indexOf(".") > -1 && // 存在.
+        money.lastIndexOf(".") + 1 === money.length // .在最后一位
+      ) {
+        money = money.substring(0, money.lastIndexOf(".")); // 截取最后一位.
+      }
+
+      money += "+";
     } else if (key === "再记") {
-      this.props.submitAgain()
+      // 再记
+      this.props.submitAgain();
+      return;
     } else if (key === "保存") {
-      money = ""
-      this.props.submitSave()
+      // 保存
+      money = "";
+      this.props.submitSave();
+      return;
     }
+    this.props.moneyChange(money);
   }
 
   render() {
